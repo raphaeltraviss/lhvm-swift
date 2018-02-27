ef326715d39abb14e172d59c0b26ed5e57132b7d
Raphael Traviss <raphael@skyleafdesign.com>
2018-02-27 12:31:58 -0600

    Initial Commit

---
+++ lhvm-swift/lhvm_swift.h
@@ -0,0 +1,19 @@
+//
+//  lhvm_swift.h
+//  lhvm-swift
+//
+//  Created by Raphael on 2/27/18.
+//  Copyright © 2018 Skyleaf Design LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for lhvm_swift.
+FOUNDATION_EXPORT double lhvm_swiftVersionNumber;
+
+//! Project version string for lhvm_swift.
+FOUNDATION_EXPORT const unsigned char lhvm_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lhvm_swift/PublicHeader.h>
+
+
